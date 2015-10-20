f65ff4c6aa5cfad81cfe432f1fd0d0cbd84944de
Rob Haswell <me@robhaswell.co.uk>
2015-10-20 16:56:17 +0100

    Force-overwrite the sde.

--- sde_update.sh
+++ sde_update.sh
@@ -3,7 +3,7 @@
 SDE_URL="https://www.fuzzwork.co.uk/dump/sqlite-latest.sqlite.bz2"
 
 echo "Downloading SDE from ${SDE_URL}"
-curl -o sde.sqlite.bz2 "${SDE_URL}" && bunzip2 sde.sqlite.bz2
+curl -o sde.sqlite.bz2 "${SDE_URL}" && bunzip2 -f sde.sqlite.bz2
 if [ -e sde.sqlite ]; then
     echo "Generating reference data"
     /usr/bin/env python gen_reference_data.py sde.sqlite
